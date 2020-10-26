07f1d77dc17f13d1899f5bc3cf905b24d6ab82d7
anhpnhhcm <68736237+anhpnhhcm@users.noreply.github.com>
2020-10-26 23:51:54 +0700

    show du lieu

--- src/main/java/vn/edu/poly/beecinema/service/SukienService.java
+++ src/main/java/vn/edu/poly/beecinema/service/SukienService.java
@@ -1,4 +1,16 @@
 package vn.edu.poly.beecinema.service;
 
+import vn.edu.poly.beecinema.entity.Sukien;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SukienService {
+    List<Sukien> getAllSukien();
+
+    void saveSukien(Sukien sukien);
+
+    void deleteSukien(String id);
+
+    Optional<Sukien> findSukienById(String id);
 }
