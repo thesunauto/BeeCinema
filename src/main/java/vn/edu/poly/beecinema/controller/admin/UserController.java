22bb09ba45501630704c79228e9310d2bbf050b8
chaundhcm <chaundhcm@gmail.com>
2020-10-28 00:16:52 +0700

    sua file UserController

--- src/main/java/vn/edu/poly/beecinema/controller/admin/UserController.java
+++ src/main/java/vn/edu/poly/beecinema/controller/admin/UserController.java
@@ -1,14 +1,26 @@
 package vn.edu.poly.beecinema.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vn.edu.poly.beecinema.entity.DoTuoi;
+import vn.edu.poly.beecinema.entity.Taikhoan;
+import vn.edu.poly.beecinema.service.TaikhoanService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/user")
 public class UserController {
-    @RequestMapping("/show-user")
+    @Autowired
+    private TaikhoanService taikhoanService;
+
+    @GetMapping("/show-user")
     public String showUser(Model model){
+        List<Taikhoan> taikhoan = taikhoanService.getAllTaikhoan();
+        model.addAttribute("taikhoans", taikhoan);
         return "admin/account/show-account";
     }
 
