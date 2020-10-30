54bc4dd0f04ff432d23eb8ae239160d7167e212b
thesunautomt <thesunautomt@gmail.com>
2020-10-30 17:26:26 +0700

    tạm hoàn thành đăng nhập

--- src/main/java/vn/edu/poly/beecinema/controller/MainController.java
+++ src/main/java/vn/edu/poly/beecinema/controller/MainController.java
@@ -1,15 +1,36 @@
 package vn.edu.poly.beecinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import vn.edu.poly.beecinema.service.QuyenService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
     @Autowired
     private QuyenService quyenService;
+
+    @RequestMapping("/becinema")
+    public String userHomePage(Model model, Authentication authentication) {
+        String path="";
+        System.out.println(authentication.getAuthorities());
+        boolean hasUSERRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_USER"));
+        boolean hasADMINRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
+        boolean hasEMPRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_EMP"));
+        if(hasADMINRole){path= "redirect:/admin/user/show-user";};
+        if(hasEMPRole) {path=  "redirect:/employee/datve";};
+        if(hasUSERRole){path=  "redirect:/";};
+        return path;
+    }
 
     @RequestMapping("/")
     public String userHomePage(Model model) {
@@ -18,6 +39,12 @@ public class MainController {
 
     @RequestMapping("/login")
     public String loginPage(Model model) {
+        model.addAttribute("message","");
+        return "client/SignIn";
+    }
+    @RequestMapping("/loginfail")
+    public String loginfPage(Model model) {
+        model.addAttribute("message","toastr.error('Tài khoản mật khẩu không đúng', '', {positionClass: 'md-toast-top-right'});$('#toast-container').attr('class','md-toast-top-right');");
         return "client/SignIn";
     }
     @RequestMapping("/signup")
