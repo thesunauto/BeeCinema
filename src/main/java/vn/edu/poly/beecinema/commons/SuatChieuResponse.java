fa9caf1de19c8f1f15a46b73f1cc3c99a7586ac8
thesunautomt <thesunautomt@gmail.com>
2020-10-31 11:57:48 +0700

    employee datve

---
+++ src/main/java/vn/edu/poly/beecinema/commons/SuatChieuResponse.java
@@ -0,0 +1,9 @@
+package vn.edu.poly.beecinema.commons;
+
+public class SuatChieuResponse {
+    private Integer id;
+    private String idphim;
+    private String idphong;
+    private String batdau;
+    private Double dongia;
+}
--- src/main/java/vn/edu/poly/beecinema/controller/rest/EmployeeRestController.java
+++ src/main/java/vn/edu/poly/beecinema/controller/rest/EmployeeRestController.java
@@ -1,15 +1,44 @@
 package vn.edu.poly.beecinema.controller.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.edu.poly.beecinema.commons.PhimResponse;
+import vn.edu.poly.beecinema.service.PhimService;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeRestController {
-    @PostMapping("/list-film-showing")
+    @Autowired private PhimService phimService;
+    @GetMapping("/list-film-showing")
     public ResponseEntity listFilmShowing(){
-        return ResponseEntity.ok().body("hello");
+        List<PhimResponse> phimResponses = new ArrayList<>();
+        phimService.getAllPhim().forEach(value->{
+            PhimResponse phim = new PhimResponse();
+            List<Integer> idsuatchieu = new ArrayList<>();
+            value.getSuatchieus().forEach(suatchieu -> {
+                idsuatchieu.add(suatchieu.getId());
+            });
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            phim.setId(value.getId());
+            phim.setDotuoi(value.getDotuoi().getTen());
+            phim.setNgonngu(value.getNgonngu().getTen());
+            phim.setLoaiphim(value.getLoaiphim().getTen());
+            phim.setTen(value.getTen());
+            phim.setHinhanh(value.getHinhanh());
+            phim.setNgaybatdau(value.getNgaybatdau().format(dateTimeFormatter));
+            phim.setNgayketthuc(value.getNgayketthuc().format(dateTimeFormatter));
+            phim.setDodai(value.getDodai());
+            phim.setIdsuatchieu(idsuatchieu);
+            phimResponses.add(phim);
+        });
+        return ResponseEntity.ok().body(phimResponses);
     }
 }
