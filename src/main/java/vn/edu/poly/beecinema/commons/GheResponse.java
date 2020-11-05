629fb8b5a156e4c5f15f356909bd621c88f8c060
anhpnhhcm <68736237+anhpnhhcm@users.noreply.github.com>
2020-11-05 16:46:54 +0700

    update gheResponsive

--- src/main/java/vn/edu/poly/beecinema/commons/GheResponse.java
+++ src/main/java/vn/edu/poly/beecinema/commons/GheResponse.java
@@ -18,7 +18,9 @@ public class GheResponse {
     /**
      * @trangthai = 0 ghế sẳn sàng
      * @trangthai = 1 ghế đã có vé
-     * @trangthai = 2 ghế đang được chọn
-     * @trangthai = 3 ghế không thể chọn là đường đi hay gì đó
+     * @trangthai = 2 ghế mình đang chọn
+     * @trangthai = 3 ghế người khác đang chọn
+     * @trangthai = 4 ghế không thể chọn là đường đi hay gì đó
+     *
      */
 }
