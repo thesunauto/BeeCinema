d770b139161c9b108579aace8ad0674dd6a35787
thesunautomt <thesunautomt@gmail.com>
2020-11-12 15:39:39 +0700

    fix ghe

--- src/main/java/vn/edu/poly/beecinema/config/WebSecurityConfig.java
+++ src/main/java/vn/edu/poly/beecinema/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,29 +15,39 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import vn.edu.poly.beecinema.entity.Taikhoan;
 import vn.edu.poly.beecinema.service.TaikhoanService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends  WebSecurityConfigurerAdapter {
     @Autowired
     TaikhoanService taikhoanService;
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    {
+        List<UserDetails> userDetailsList = new ArrayList<>();
         List<Taikhoan> taikhoans = taikhoanService.getAllTaikhoan();
         taikhoans.forEach(user -> {
-            manager.createUser(User.withDefaultPasswordEncoder()
+            userDetailsList.add(User.withDefaultPasswordEncoder()
                     .username(user.getUsername())
                     .password(user.getMatkhau())
                     .roles(user.getQuyen().getTen())
                     .build());
         });
-        return manager;
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/css/**", "/js/**", "/img/**","/api/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/employee/**").hasRole("EMP")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
