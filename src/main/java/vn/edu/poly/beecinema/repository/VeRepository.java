c0ac8b040e1b37bbf5c6d5b8970c402cc4264633
chaundhcm <chaundhcm@gmail.com>
2020-11-19 13:36:44 +0700

    .

--- src/main/java/vn/edu/poly/beecinema/repository/VeRepository.java
+++ src/main/java/vn/edu/poly/beecinema/repository/VeRepository.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import vn.edu.poly.beecinema.entity.Ghe;
-import vn.edu.poly.beecinema.entity.LoaiPhim;
-import vn.edu.poly.beecinema.entity.Suatchieu;
-import vn.edu.poly.beecinema.entity.Ve;
+import vn.edu.poly.beecinema.entity.*;
 
 import java.util.List;
 
